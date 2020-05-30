f7dfdc122e012bdbcd63ebad88bc2f60caf5962f
Altus Carstens <carstensaltus@gmail.com>
2020-05-30 18:43:03 +0200

    fix refs

--- ouput.tf
+++ ouput.tf
@@ -1,7 +1,7 @@
 output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.website_cdn.domain_name
+  value = aws_cloudfront_distribution.cloudfront.domain_name
 }
 
 output "cloudfront_hosted_zone_id" {
-  value = aws_cloudfront_distribution.website_cdn.hosted_zone_id
+  value = aws_cloudfront_distribution.cloudfront.hosted_zone_id
 }
